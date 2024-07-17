7cf4fa088e274f32bca8e2dbe81858b9aa3eee0e
yashiwashi123 <hashemiyashar@gmail.com>
2024-07-17 19:40:07 -0400

    latro_emu

--- resources/latrodectus/conn_test.py
+++ resources/latrodectus/conn_test.py
@@ -1,34 +1,200 @@
 import requests
-real_url = ""
-url = "http://example.com"
-headers = {
-    'User-Agent': "Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.10136",
-    'Accept-Encoding': "",
-    'Content-Type': 'application/octect-stream',
-    'Authorization': 'Basic MzM5MzQzNjMwMzozOTk3MDYwMTkwOjEyMDo2NjoxNg==',
-    'Cookie': 'session=MDowOjA6MTI5ODc6MA='
-    }
+import fnv
+from Crypto.Cipher import ARC4
+import base64
+import random
+import string
+from faker import Faker
+from time import sleep
+import warnings
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
 
-'''
-Enrcryption Key: qNfSHTVKEU7mknHSFrQCwp0mmQfXUNPIcA66gezNz49qQOVX0P
+# Suppress only the single InsecureRequestWarning from urllib3
+warnings.simplefilter('ignore', InsecureRequestWarning)
+#url = "https://astopertat.com/live/"
+#Enrcryption Key: qNfSHTVKEU7mknHSFrQCwp0mmQfXUNPIcA66gezNz49qQOVX0P
 
-Things to set: 
 
-Group
-Version
-Direction
+class latro_bot: 
+    def __init__(self, key, campaign_id, url, counter = None):
+        self.key = key
+        self.campaign_id = campaign_id
+        self.url = url
+        if not counter: 
+            self.counter = 0
+        else: 
+            self.counter = counter
+        self.headers = {
+        'User-Agent': "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Tob 1.1)",
+        'Accept-Encoding': "",
+        'Content-Type': 'application/octect-stream',
+        }
+        self.campaign_id_hash()
+        self.generate_hex_string()
+        self.generate_fake_user()
+        self.command_dict = {
+            '2':True,
+            '3':True,
+            '4':True,
+            '20':self.command_20_handle
+        }
+        self.previous_command = None
 
-'''
+    def campaign_id_hash(self): 
+        self.campaign_fnva = fnv.hash(self.campaign_id.encode("utf-8"), bits=32)
+        #return project_fnva
+    
+    def generate_hex_string(self, length=28): 
+        hex_chars = string.hexdigits.upper()
+        self.bot_id = ''.join(random.choices(hex_chars, k=length)) 
+    
+    def generate_fake_user(self): 
+        fake = Faker()
+        self.username = fake.user_name()
 
-'''
+    
+    def packet_format_string(self, username=None, type_ = 1, os=6, arch=1, ver_major=1, ver_minor=3, up=2, direction=None, guid = None, counter = None, packet_type= None):
+        self.campaign_id_hash()
+        group = self.campaign_fnva
+        if not guid:
+            guid = self.bot_id  
+        if not counter: 
+            counter = self.counter
+        if not direction: 
+            direction = self.url
+        if not username: 
+            username = self.username
+        
+        standard_packet =  f"counter={counter}&type={type_}&guid={guid}&os={os}&arch={arch}&username={username}&group={group}&ver={ver_major}.{ver_minor}&up={up}&direction={direction}"
+        if packet_type == '2': 
+            final_packet = standard_packet + '&desklinks=["OneDrive.lnk","OneNote.lnk","PowerPoint.lnk","Excel.lnk","Google Chrome.lnk","Notepad.lnk","Paint.lnk"]'
+            return final_packet
+        if packet_type == '4': 
+            command_4_ext = self.command_4_handle()
+            final_packet = standard_packet + command_4_ext
+            return final_packet
+        
+        else: 
+            return standard_packet
 
-counter=0&type=1&guid=01B10D63A93595915DB96521ADC9&os=6&arch=1&username=yashiwashi&group=2296796206&ver=1.3&up=2&direction=https://ultroawest.com/live/
+        
 
-rc4 and 64 : hcyeNY2LALy8fz5kBNZjkK+KOo+UvgK8nSBRO5D9ZpMQ0OJqPB/lQJGirCtvXbUQKQ3SzTnHiCOX+2Mf1KJ8GNUPfADz3RvcjGDPEOOgj6gysVDDnaf8QEKc/47tYX6Jh0WtWB1by2frDUXLamFv/I2OnnZZYK++tOLXcGIRic0TDY8bUCTGV5TlRxWDFtnHgHtczx51hg==
-'''
+    def test_registration_packet_data(self):
+        print(self.packet_format_string(packet_type= '4'))
+        print(self.packet_format_string(packet_type= '2'))
+        print(self.packet_format_string())
+    
+    def crypt_packet_data(self, data, key=None): 
+        if not key: 
+            key = self.key
+        cipher = ARC4.new(key.encode())
+        encrpyted = cipher.encrypt(data.encode())
+        encoded = base64.b64encode(encrpyted).decode()
+        return encoded
 
-data = "test"
-data = data.encode('utf-8')
+    def next_packet(self): 
+        self.counter += 1
 
-r = requests.post(url, headers = headers, data = data)
-print (r.text)
+
+    def registration_post_request(self, packet_data):
+        data = packet_data
+        print(data)
+        data = self.crypt_packet_data(data)
+        print(data)
+        r = requests.post(self.url, headers = self.headers, data = data, verify=False)
+        print(r.text)
+        return r.text
+
+    def decrypt_packet(self, packet): 
+        decoded_data = base64.b64decode(packet)
+     
+        cipher = ARC4.new(self.key.encode())
+        decrypted_data = cipher.decrypt(decoded_data)
+        decrypted_string = decrypted_data.decode('utf-8')
+        
+        return decrypted_string
+    
+    def command_3_handle(self):
+        pass
+    
+    def command_4_handle(self):
+        """
+        ipconfig=
+        systeminfo=
+        domain_trusts=
+        domain_trusts_all=
+        net_view_all_domain=
+        net_view_all=
+        net_group=
+        wmic=
+        net_config_ws=
+        net_wmic_av=
+        whoami_group=
+        """
+        packet = ""
+        packet += "ipconfig=%s&" % self.get_replaced_template("ipconfig")
+        packet += "systeminfo=%s&" % self.get_replaced_template("systeminfo")
+        packet += "domain_trusts=%s&" % self.get_replaced_template("domain_trusts")
+        packet += "domain_trusts_all=%s&" % self.get_replaced_template(
+            "domain_trusts_all"
+        )
+        packet += "net_view_all_domain=%s&" % self.get_replaced_template(
+            "net_view_all_domain"
+        )
+        packet += "net_view_all=%s&" % self.get_replaced_template("net_view_all")
+        packet += "net_group=%s&" % self.get_replaced_template("net_group")
+        packet += "wmic=%s&" % self.get_replaced_template("wmic")
+        packet += "net_config_ws=%s&" % self.get_replaced_template("net_config_ws")
+        packet += "net_wmic_av=%s&" % self.get_replaced_template("net_wmic_av")
+        packet += "whoami_group=%s" % self.get_replaced_template("whoami_group")
+
+        return packet
+    
+    def get_replaced_template(self, command_name):
+        data = ""
+        with open("command_templates/%s_example.txt" % command_name, "r") as f:
+            data = f.read()
+
+        data = data.replace("{{USERNAME}}", self.username)
+        data = data.replace("{{HOSTNAME}}", self.username)
+        data = data.replace("{{DOMAINAME}}", self.username)
+        data = base64.b64encode(data.encode("utf-8")).decode("utf-8")
+        return data
+
+    def command_20_handle(self):
+        self.counter = 0
+    
+    def comms_command_handler(self, decrypted_data= None): 
+            command_list = decrypted_data.split('|')
+            command_num = command_list[1]
+            if command_num in self.command_dict: 
+                if self.command_dict[command_num] == True: 
+                    self.previous_command == command_num
+                else:
+                    self.command_dict[command_num]()
+            else: 
+                pass
+    
+    def comms_main(self): 
+        while True: 
+            if self.previous_command: 
+
+                packet_data = self.packet_format_string(packet_type= self.previous_command)
+                self.previous_command = None
+            else: 
+                packet_data = self.packet_format_string()
+            response_data = self.registration_post_request(packet_data)
+            self.next_packet()
+            decrypted_data = self.decrypt_packet(response_data)
+            print(decrypted_data)
+            self.comms_command_handler(decrypted_data)
+            sleep(37)
+
+
+
+
+
+#test_bot = latro_bot(key= 'qNfSHTVKEU7mknHSFrQCwp0mmQfXUNPIcA66gezNz49qQOVX0P', campaign_id='Jupiter', url='https://riscoarchez.com/live/')
+test_bot = latro_bot(counter = 0, key= 'EhAyPSHvva9CvL6OIddDJvDXHJjoMsqXyjraKyYmXFqDGdAYyO', campaign_id='Venus', url='https://stripplasst.com/live/')
+print(test_bot.counter)
+test_bot.comms_main()
